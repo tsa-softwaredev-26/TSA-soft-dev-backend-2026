c27019590e7135d7d022263418530a400ac99149
Joseph Roch <jdroche28@gmail.com>
2026-01-15 17:45:53 -0500

    chore: initial project setup
    - Create project repository and basic structure

---
+++ VisualMemoryIOS/VisualMemoryIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualMemoryIOSApp.swift
+//  VisualMemoryIOS
+//
+//  Created by Joe Roche on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct VisualMemoryIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
